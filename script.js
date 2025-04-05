ff66e1dd2a8aef9f647714e63a23b8f919632d63
DevDiscere <dev.ginomiguel@gmail.com>
2025-04-06 01:52:09 +0800

    Create basic displayImagePreview function
    For the HTML, I added a label and an input for the image file uploader.
    It accepts multiple files of jpg and png image types. I also added a div
    that would contain the image previews of the uploaded images.
    
    For the CSS, I set the opacity of the image preview div to zero so that
    it is "hidden" when the images are not yet uploaded.
    
    For the JS, I created a basic logic of the displayImagePreview function.
    It is composed of three logic: the first clears the image preview so
    that when the user sets another image, it does not stack. The second
    dynamically creates the images uploaded if they pass the condition. And,
    the third warns the user that they exceed max uploaded images.

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-para = document.querySelector("p");
-para.style.fontSize = "3rem";
+const displayImagePreview = function displayImagePreview () {
+    const maxUploadedImages = 5;
+    const uploadedImages = fileUploader.files;
+    const imagePreview = document.querySelector(".image-preview");
+
+    // Clear images of image preview
+    while (imagePreview.firstChild) {
+        imagePreview.removeChild(imagePreview.firstChild);
+    }
+
+    if (uploadedImages.length != 0 && uploadedImages.length <= maxUploadedImages) {
+        test = uploadedImages;
+        test2 = fileUploader.value;
+        imagePreview.style.opacity = 1;
+
+        for (const imageFile of uploadedImages) {
+            const image = document.createElement("img");
+
+            image.src = URL.createObjectURL(imageFile);
+            image.alt = imageFile.name;
+            imagePreview.appendChild(image);
+        }
+
+    } else if (uploadedImages.length > maxUploadedImages) {
+        fileUploader.value = "";
+        alert(`You uploaded images more than the maximum upload of ${maxUploadedImages} images`);
+    }
+    // Edge case where the fileUploader clears the uploaded images because user cancels adding files.
+}
+
+const fileUploader = document.querySelector("#file-uploader");
+
+fileUploader.addEventListener("change", displayImagePreview);
+fileUploader.addEventListener("cancel", () => alert("Cancelled"));
